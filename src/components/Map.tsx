a0d7a112b1ad95e1ceb53f5756bd0bf779c499d1
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-09 22:26:02 +0500

    add boilerplate

---
+++ src/components/Map.tsx
@@ -0,0 +1,13 @@
+import Logo from "./Logo";
+
+function Map() {
+  return (
+    <div className=" bg-colorDark-2 flex flex-col justify-center items-center">
+      <div>
+        <Logo />
+      </div>
+    </div>
+  );
+}
+
+export default Map;
