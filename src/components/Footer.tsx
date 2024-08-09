d4884d06a6055b79b0c4418502420a70573dd8dd
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-09 22:26:13 +0500

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="text-colorLight-1 text-sm font-medium">
+        &copy; Copyright 2024 by WorldWise Inc.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
