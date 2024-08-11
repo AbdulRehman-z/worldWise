420a955d7f1a7a9bc0b81cc2aa2baa0f1200930c
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-11 23:17:20 +0500

    Refactor City component to use CitiesContext for managing cities data and add lazy loading for images

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -1,21 +1,20 @@
-import { CitiesArrayType } from "../schema";
+import { Outlet } from "react-router-dom";
 import CityItem from "./CityItem";
 import Message from "./Message";
+import { useCitiesContext } from "../contexts/CitiesContext";
+import Spinner from "./Spinner";
 
-type CityProps = {
-  isLoading: boolean;
-  cities: CitiesArrayType;
-};
+function City() {
+  const { cities, isLoading } = useCitiesContext();
 
-function City({ cities, isLoading }: CityProps) {
-  if (isLoading)
-    return <p className="mt-16 text-xl text-colorLight-3">Cities Loading...</p>;
+  if (isLoading) return <Spinner />;
   if (cities.length === 0)
     return (
       <Message message="Select your first city by clicking on a city on the map." />
     );
   return (
     <div className="mt-8 w-96">
+      <Outlet />
       {cities.map((city) => (
         <CityItem key={city.id} city={city} />
       ))}
