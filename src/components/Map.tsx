e873adf0e7252bf27b5737a45c02e9450250e973
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-11 23:15:56 +0500

    Refactor Map component to use react-leaflet and add geolocation functionality

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,13 +1,102 @@
-import Logo from "./Logo";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useCitiesContext } from "../contexts/CitiesContext";
+import { useEffect, useState } from "react";
+import { useGeolocation } from "../hooks/useGeoLocation";
 
-function Map() {
+function Map(): JSX.Element {
+  const [searchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState({ lat: 40, lng: 0 });
+  const {
+    getPosition,
+    isLoading: isGeoPositionLoading,
+    position: geoLocationPosition,
+  } = useGeolocation();
+
+  const { cities } = useCitiesContext();
+
+  const lat = Number(searchParams.get("lat"));
+  const lng = Number(searchParams.get("lng"));
+
+  useEffect(() => {
+    if (lat && lng) {
+      setMapPosition({ lat, lng });
+    }
+  }, [lat, lng]);
+
+  useEffect(() => {
+    if (geoLocationPosition) {
+      setMapPosition({
+        lat: geoLocationPosition.lat,
+        lng: geoLocationPosition.lng,
+      });
+    }
+
+    console.log(mapPosition);
+  }, [geoLocationPosition]);
+
   return (
-    <div className=" bg-colorDark-2 flex flex-col justify-center items-center">
-      <div>
-        <Logo />
-      </div>
+    <div className="h-full relative ">
+      {!geoLocationPosition && (
+        <button className="btn-primary" onClick={getPosition}>
+          {isGeoPositionLoading
+            ? "Getting Your Position..."
+            : "Use Your Position"}
+        </button>
+      )}
+
+      <MapContainer
+        className="h-full bg-colorDark-2"
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://wwgw.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            key={city.id}
+            position={[city.position.lat, city.position.lng]}
+          >
+            <Popup>
+              {city.emoji} {city.country}.
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer>
     </div>
   );
+}
+
+type ChangeCenterProps = {
+  position: { lat: number; lng: number };
+};
+
+function ChangeCenter({ position }: ChangeCenterProps) {
+  const map = useMap();
+  map.setView([position.lat, position.lng], map.getZoom());
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
+
+  return null;
 }
 
 export default Map;
