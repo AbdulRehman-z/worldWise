ad2692bec88455bcd63feb55366b6640f04ae178
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-11 23:17:57 +0500

    Refactor AppNav component for improved readability and consistency

--- src/components/AppNav.tsx
+++ src/components/AppNav.tsx
@@ -5,14 +5,14 @@ const AppNav = () => {
     <nav className="bg-colorDark-2 rounded-md flex items-center mt-16">
       <NavLink
         to="cities"
-        className={`px-7 py-1 rounded-md shadow-md focus:bg-colorDark-0 transition-all duration-100 ease-linear
-		`}
+        className={`px-7 py-1 rounded-md shadow-md focus:bg-colorDark-0  transition-all duration-100 ease-linear
+    `}
       >
         Cities
       </NavLink>
       <NavLink
         to="countries"
-        className={`px-7 py-1 rounded-md shadow-md 	focus:bg-colorDark-0 transition-all duration-100 ease-linear`}
+        className={`px-7 py-1 rounded-md shadow-md focus:bg-colorDark-0  transition-all duration-100 ease-linear`}
       >
         Countries
       </NavLink>
