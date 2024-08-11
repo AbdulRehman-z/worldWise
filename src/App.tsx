b1e0890b90c006046e74c1b9205dd716526b14e6
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-11 23:19:18 +0500

    Refactor App component to use CitiesContext for managing cities data, add CityDetails and Form components, and update routing with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -6,58 +6,31 @@ import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import City from "./components/City";
-import { useEffect, useState } from "react";
-import { CitiesArrayType, citiesSchema } from "./schema";
 import Country from "./components/Country";
+import CityDetails from "./components/CityDetails";
+import Form from "./components/Form";
+import CitiesContextProvider from "./contexts/CitiesContext";
 
 function App() {
-  const [cities, setCities] = useState<CitiesArrayType | []>([]);
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    async function fetchCities() {
-      try {
-        setLoading(true);
-
-        const response = await fetch("http://localhost:3000/cities");
-        const data = await response.json();
-        const validateData = citiesSchema.safeParse(data);
-        if (!validateData.success) {
-          throw new Error(validateData.error.message);
-        }
-        console.log(validateData.data);
-        setCities(validateData.data);
-      } catch (error) {
-        alert(`Error: ${error}`);
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<City cities={cities} isLoading={isLoading} />}
-          />
-          <Route
-            index
-            path="cities"
-            element={<City cities={cities} isLoading={isLoading} />}
-          />
-          <Route path="countries" element={<Country cities={cities} />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <CitiesContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route path="cities" element={<City />} />
+            <Route path="countries" element={<Country />} />
+            <Route path="cities/:id" element={<CityDetails />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesContextProvider>
   );
 }
 
