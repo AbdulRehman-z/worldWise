907f5e52cda1d2ddce2c74315b42e68ce712fee8
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-13 20:13:18 +0500

    add schema

--- src/schema.ts
+++ src/schema.ts
@@ -15,6 +15,52 @@ export const citySchema = z.object({
   position: positionSchema,
 });
 
+const LocalityInfoAdministrativeItemSchema = z.object({
+  adminLevel: z.number().int(),
+  description: z.string().optional(),
+  geonameId: z.number().int().optional(),
+  isoCode: z.string().optional(),
+  isoName: z.string().optional(),
+  name: z.string(),
+  order: z.number().int(),
+  wikidataId: z.string().optional(),
+});
+
+const LocalityInfoInformativeItemSchema = z.object({
+  description: z.string().optional(),
+  geonameId: z.number().int().optional(),
+  isoCode: z.string().optional(),
+  isoName: z.string().optional(),
+  name: z.string(),
+  order: z.number().int(),
+  wikidataId: z.string().optional(),
+});
+
+const LocalityInfoSchema = z.object({
+  administrative: z.array(LocalityInfoAdministrativeItemSchema),
+  informative: z.array(LocalityInfoInformativeItemSchema),
+});
+
+const DataSchema = z.object({
+  city: z.string().min(1),
+  continent: z.string(),
+  continentCode: z.string(),
+  countryCode: z.string(),
+  countryName: z.string(),
+  latitude: z.number(),
+  locality: z.string(),
+  localityInfo: LocalityInfoSchema,
+  localityLanguageRequested: z.string(),
+  longitude: z.number(),
+  lookupSource: z.string(),
+  plusCode: z.string(),
+  postcode: z.string(),
+  principalSubdivision: z.string(),
+  principalSubdivisionCode: z.string(),
+});
+
+export default DataSchema;
+
 export const citiesSchema = z.array(citySchema);
 export type CitiesArrayType = z.infer<typeof citiesSchema>;
 export type CityType = z.infer<typeof citySchema>;
