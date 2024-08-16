823ed8239da192fe91057b0c7a1f81eec9d03431
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-16 21:35:49 +0500

    Lazy load pages and components for improved performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import NotFound from "./pages/NotFound";
-import Login from "./pages/Login";
-import AppLayout from "./pages/AppLayout";
 import City from "./components/City";
 import Country from "./components/Country";
 import CityDetails from "./components/CityDetails";
@@ -13,32 +8,47 @@ import CitiesContextProvider from "./contexts/CitiesContext";
 import { AuthContextProvider } from "./contexts/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+
 function App() {
   return (
     <AuthContextProvider>
       <CitiesContextProvider>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="product" element={<Product />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="login" element={<Login />} />
-            <Route
-              path="app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate replace to="cities" />} />
-              <Route path="cities" element={<City />} />
-              <Route path="countries" element={<Country />} />
-              <Route path="cities/:id" element={<CityDetails />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <Suspense
+            fallback={
+              <div className="text-4xl text-colorDark-1 font-bold flex justify-center items-center b-2">
+                Loading...
+              </div>
+            }
+          >
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<City />} />
+                <Route path="countries" element={<Country />} />
+                <Route path="cities/:id" element={<CityDetails />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesContextProvider>
     </AuthContextProvider>
