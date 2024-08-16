8b48184837805ae1b61b94e7a697da7314384538
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-16 20:12:59 +0500

    Update routing structure and add authentication support

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,38 @@ import Country from "./components/Country";
 import CityDetails from "./components/CityDetails";
 import Form from "./components/Form";
 import CitiesContextProvider from "./contexts/CitiesContext";
+import { AuthContextProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <CitiesContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<Navigate replace to="cities" />} />
-            <Route path="cities" element={<City />} />
-            <Route path="countries" element={<Country />} />
-            <Route path="cities/:id" element={<CityDetails />} />
-            <Route path="form" element={<Form />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CitiesContextProvider>
+    <AuthContextProvider>
+      <CitiesContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate replace to="cities" />} />
+              <Route path="cities" element={<City />} />
+              <Route path="countries" element={<Country />} />
+              <Route path="cities/:id" element={<CityDetails />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesContextProvider>
+    </AuthContextProvider>
   );
 }
 
